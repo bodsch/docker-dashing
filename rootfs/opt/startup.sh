edbdd428a487bf3c12706dfb8b797ac1f8a5d665
Bodo Schulz <bodo.schulz@coremedia.com>
2016-03-02 17:08:19 +0100

    read configuration from /opt/dashing-icinga2/jobs/icinga2.rb

--- rootfs/opt/startup.sh
+++ rootfs/opt/startup.sh
@@ -4,7 +4,7 @@ initfile=/opt/run.init
 
 AUTH_TOKEN=${AUTH_TOKEN:-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)}
 ICINGA2_HOST=${ICINGA2_HOST:-"localhost"}
-ICINGA2_PORT=${ICINGA2_PORT:-"5225"}
+ICINGA2_PORT=${ICINGA2_PORT:-"5665"}
 ICINGA2_DASHING_APIUSER=${ICINGA2_DASHING_APIUSER:-"dashing"}
 ICINGA2_DASHING_APIPASS=${ICINGA2_DASHING_APIPASS:-"icinga"}
 
@@ -14,11 +14,12 @@ then
 
   if [ -f /opt/dashing-icinga2/config.ru ]
   then
-    sed -i 's,%AUTH_TOKEN%,'${AUTH_TOKEN}',g'                 /opt/dashing-icinga2/config.ru
-    sed -i 's/%ICINGA2_HOST%/'${ICINGA2_HOST}'/g'                        /opt/dashing-icinga2/config.ru
-    sed -i 's/%ICINGA2_PORT%/'${ICINGA2_PORT}'/g'                        /opt/dashing-icinga2/config.ru
-    sed -i 's/%ICINGA2_DASHING_APIUSER%/'${ICINGA2_DASHING_APIUSER}'/g'  /opt/dashing-icinga2/config.ru
-    sed -i 's/%ICINGA2_DASHING_APIPASS%/'${ICINGA2_DASHING_APIPASS}'/g'  /opt/dashing-icinga2/config.ru
+    sed -i 's,%AUTH_TOKEN%,'${AUTH_TOKEN}',g'                            /opt/dashing-icinga2/config.ru
+    
+    sed -i 's/%ICINGA2_HOST%/'${ICINGA2_HOST}'/g'                        /opt/dashing-icinga2/jobs/icinga2.rb
+    sed -i 's/%ICINGA2_PORT%/'${ICINGA2_PORT}'/g'                        /opt/dashing-icinga2/jobs/icinga2.rb
+    sed -i 's/%ICINGA2_DASHING_APIUSER%/'${ICINGA2_DASHING_APIUSER}'/g'  /opt/dashing-icinga2/jobs/icinga2.rb
+    sed -i 's/%ICINGA2_DASHING_APIPASS%/'${ICINGA2_DASHING_APIPASS}'/g'  /opt/dashing-icinga2/jobs/icinga2.rb
 
     sed -i 's|bash|sh|g' /opt/dashing-icinga2/run.sh
   fi
